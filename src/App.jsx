32d4f0437bccedb9c21884efa680e334414ae397
JazielA <jaz.aguilera@duocuc.cl>
2025-01-12 19:40:22 -0300

    Add weather app form with city input and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,87 @@
+//visualizo la variable de entorno en consola
+//console.log(import.meta.env.VITE_API_KEY);
+
+import { LoadingButton } from "@mui/lab";
+import { Box, Container, Typography, TextField } from "@mui/material";
+import { useState } from "react";
+
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
+  import.meta.env.VITE_API_KEY
+}&q=`;
+
 export default function app() {
+  //Estados
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Formulario enviado");
+    setLoading(true);
+    setError({
+      error: false,
+      message: "",
+    });
+    try {
+      if (!city.trim())
+        throw {
+          message: "El campo ciudad es requerido",
+        };
+    } catch (error) {
+      console.error(error);
+      setError({
+        error: true,
+        message: error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="xs" sx={{ mt: 2 }}>
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        Weather App
+      </Typography>
+      <Box
+        sx={{ display: "grid", gap: 2 }}
+        component="form"
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id="city"
+          label="Ciudad"
+          variant="outlined"
+          size="small"
+          required
+          fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          error={error.error}
+          helperText={error.message}
+        />
+
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={loading}
+          loadingIndicator="Cargando..."
+        >
+          Buscar
+        </LoadingButton>
+      </Box>
+
+      <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px" }}>
+        Powered by:{" "}
+        <a href="https://www.weatherapi.com/" title="Weather API">
+          WeatherAPI.com
+        </a>
+      </Typography>
+    </Container>
   );
 }
