b6795ae985488d6b9a4106c4f594252b288b41e8
JazielA <jaz.aguilera@duocuc.cl>
2025-01-12 22:57:14 -0300

    Add weather data fetching and display functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,15 @@ export default function app() {
     error: false,
     message: "",
   });
+  //capturar los datos de la api
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: "",
+  });
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -31,6 +40,21 @@ export default function app() {
         throw {
           message: "El campo ciudad es requerido",
         };
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+
+      console.log(data);
+
+      if (data.error) throw { message: data.error.message };
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.icon,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
     } catch (error) {
       console.error(error);
       setError({
@@ -75,7 +99,26 @@ export default function app() {
           Buscar
         </LoadingButton>
       </Box>
-
+      {/* mientras exista la ciudad se muestra la informacion */}
+      {weather.city && (
+        <Box sx={{ mt: 2, display: "grid", gap: 2, textAlign: "center" }}>
+          <Typography variant="h4" component="h2">
+            {weather.city}, {weather.country}
+          </Typography>
+          <Box
+            component="img"
+            alt={weather.conditionText}
+            src={weather.icon}
+            sx={{ margin: "0 auto" }}
+          ></Box>
+          <Typography variant="h5" component="h3">
+            {weather.temp}°C
+          </Typography>
+          <Typography variant="h6" component="h4">
+            {weather.conditionText}
+          </Typography>
+        </Box>
+      )}
       <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px" }}>
         Powered by:{" "}
         <a href="https://www.weatherapi.com/" title="Weather API">
